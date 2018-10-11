f7660de263c6c649a0dc8fb9225acff05de5dc08
Grigory Romodanovskiy <greg2008200@gmail.com>
2018-10-11 20:17:39 +0000

    delete old files after 60 minutes

--- start.sh
+++ start.sh
@@ -6,8 +6,8 @@ gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
 mkdir -p $DROPBOX_MOUNT_FOLDER
 inotifywait -r -m -e modify,create --format '%w%f' $DROPBOX_MOUNT_FOLDER | while read FILE
 do
+    # Delete old files
+    find ${DROPBOX_MOUNT_FOLDER}* -mmin +60 -delete
     # Copy modified file to the bucket
     gsutil cp -r $FILE gs://$GOOGLE_BUCKET
-    # Remove local copy
-    rm -r $FILE
 done
